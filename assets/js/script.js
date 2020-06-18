62f8f6bf696327668e3d9ea07b604bb6adedb16f
Regan Arnold <65038653+rarnold7@users.noreply.github.com>
2020-06-18 19:59:54 -0400

    button changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,18 @@
-var buttonEl = document.querySelector("#save-task");
+console.dir(window.document);
+
+
+
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-buttonEl.addEventListener("click", function () {
+var createTaskHandler = function (event) {
+
+    event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']");
+    console.log(taskNameInput);
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
-});
-
-var buttonEl = document.querySelector("#save-task");
-var tasksToDoEl = document.querySelector("#tasks-to-do");
-
-buttonEl.addEventListener("click", createTaskHandler);
+};
